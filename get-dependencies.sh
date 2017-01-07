15975e481266f8deee6ac7b62739f1d96bbe469f
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:54:51 -0500

    sync repositories - galaxy naming, meta

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,5 +5,11 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/golang
+[ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.mig-agentsbuild ] && ln -s ansible-mig-agentsbuild $rolesdir/juju4.mig-agentsbuild
+[ ! -e $rolesdir/juju4.mig-agentsbuild ] && cp -R $rolesdir/ansible-mig-agentsbuild $rolesdir/juju4.mig-agentsbuild
+
+## don't stop build on this script return code
+true
 
